b2a61e5fe8a1b20aff3192055a68fc09c19c323c
AndroidPayal <payalagrawal2311@gmail.com>
2018-06-12 08:12:31 +0530

    second commitment

---
+++ app/src/main/java/payal/hiteshapp/www/hiteshapp/MainActivity.java
@@ -0,0 +1,16 @@
+package payal.hiteshapp.www.hiteshapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hello payal", Toast.LENGTH_SHORT).show();
+    }
+}
